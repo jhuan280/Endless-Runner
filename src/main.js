299c12c80d3f75cc04a642c6e92dddd0211e784d
Jackie Huang <jhuan280@ucsc.edu>
2023-11-06 19:06:59 -0800

    bug

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
+            debug: true,
         }
     },
     scene: [Load, Menu, Play, GameOver, Credit]
